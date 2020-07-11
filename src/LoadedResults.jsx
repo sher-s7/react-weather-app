cd7981576f01841ec39dbb52aaeb3fd6f311ed93
Sher <sher.sheikh77@gmail.com>
2020-07-11 19:33:16 -0400

    Add more weather data to rendered city

--- src/LoadedResults.jsx
+++ src/LoadedResults.jsx
@@ -7,16 +7,37 @@ export default class LoadedResults extends React.Component {
         return
     }
 
+    unixTimeConverter(unixTime) {
+        const dateObj = new Date(unixTime * 1000);
+        const utcString = dateObj.toUTCString();
+        const time = utcString.slice(-11, -4);
+        return time;
+    }
+
+    kelvinToCelsius(temp) {
+        return Math.round(((temp - 273.15) + Number.EPSILON) * 100) / 100
+    }
+
     render() {
 
         console.log(this.props.results)
-
+        const cityList = this.props.results.list;
         return (
             <div id="loaded-results">
-                <h3 id='num-results'>Found {this.props.results.cnt} {pluralize('city',this.props.results.cnt)} named {this.props.results.list[0].name}</h3>
+                <h3 id='num-results'>
+                    Found {this.props.results.cnt} {pluralize('city', this.props.results.cnt)} named {cityList[0].name}
+                </h3>
                 <ul id='city-list'>
-                    {this.props.results.list.map((city) =>
-                        <li><span class={`flag-icon flag-icon-${city.sys.country.toLowerCase()}`}></span>{city.name}</li>
+                    {cityList.map((city) =>
+                        <li>
+                            <div className='cityContainer'>
+                                <div className='cityMainInfo'>
+                                    <span className={`flag-icon flag-icon-${city.sys.country.toLowerCase()}`}></span>{city.name}
+                                    <span className='main-temp'>{this.kelvinToCelsius(city.main.temp)}</span>
+                                    <span className='feelslike-temp'>Feels like {this.kelvinToCelsius(city.main.feels_like)}</span>
+                                </div>
+                            </div>
+                        </li>
                     )}
                 </ul>
             </div>
